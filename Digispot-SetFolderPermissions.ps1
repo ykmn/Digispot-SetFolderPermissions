66c94b01de6305f363387da62c45084b717ee8eb
Roman E <livcey@gmail.com>
2019-04-18 11:11:55 +0300

    Update Digispot-SetFolderPermissions.ps1

--- Digispot-SetFolderPermissions.ps1
+++ Digispot-SetFolderPermissions.ps1
@@ -38,8 +38,7 @@ if ($PSVersionTable.PSVersion.Major -lt 5) {
     Break
 }
 
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     # Relaunch as an elevated process:
     Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
     exit
